9eebb7a589ebfcbdf2cb2707fd4f1978bebed855
Leonardo Almeida da Silveira <leonardoas12@gmail.com>
2021-09-22 01:44:09 -0300

    Simplify NoRepeatedCharactersValidator

--- src/main/java/com/lasilveira/passwordvalidationapi/model/NoRepeatedCharactersValidator.java
+++ src/main/java/com/lasilveira/passwordvalidationapi/model/NoRepeatedCharactersValidator.java
@@ -4,27 +4,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class NoRepeatedCharactersValidator extends ChainableRuleValidator {
-
-	private Set<Character> characters;
 	
 	public NoRepeatedCharactersValidator(RuleValidator nextRule) {
 		super(nextRule);
-		initialize();
 	}
 
 	public NoRepeatedCharactersValidator() {
 		super(null);
-		initialize();
-	}
-	
-	private void initialize() {
-		this.characters = new HashSet<Character>();
 	}
 
 	@Override
 	protected boolean doValidate(String password) {
-		
-		characters.clear();
+		Set<Character> characters = new HashSet<Character>();
 		for(char c : password.toCharArray()) {
 		    if (characters.contains(c))
 		    	return false;
