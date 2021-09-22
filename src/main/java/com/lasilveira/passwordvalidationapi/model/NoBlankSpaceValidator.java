d14497a95115d5a4fa58dd8b048d7f1cb2bb6876
Leonardo Almeida da Silveira <leonardoas12@gmail.com>
2021-09-22 00:41:36 -0300

    Fix NoBlankSpaceValidator

--- src/main/java/com/lasilveira/passwordvalidationapi/model/NoBlankSpaceValidator.java
+++ src/main/java/com/lasilveira/passwordvalidationapi/model/NoBlankSpaceValidator.java
@@ -2,7 +2,7 @@ package com.lasilveira.passwordvalidationapi.model;
 
 public class NoBlankSpaceValidator extends RegExValidator {
 
-	private static final String NO_BLANK_SPACE = "^[^\s]*$";
+	private static final String NO_BLANK_SPACE = "^[^\\s]*$";
 	
 	public NoBlankSpaceValidator(RuleValidator nextRule) {
 		super(NO_BLANK_SPACE, nextRule);
